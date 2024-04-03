890cce273cc46b128cb4ef5a32ccc2fb830a79d5
West Marques <donwest@Wests-MacBook-Pro.local>
2024-04-02 19:48:47 -0700

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+-- Create the users table to store user information
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+-- Create the skate_tricks table to store skateboard trick information
+CREATE TABLE skate_tricks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    trickName VARCHAR(100) NOT NULL
+);
+
+-- Create the user_skate_tricks table to store the relationship between users and skateboard tricks
+CREATE TABLE user_skate_tricks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    trick_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (trick_id) REFERENCES skate_tricks(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+-- Seeds for Skateboard Tricks
+
+-- Beginner Level Tricks
+INSERT INTO skate_tricks (trickName) VALUES ('Ollie');
+INSERT INTO skate_tricks (trickName) VALUES ('Kickflip');
+INSERT INTO skate_tricks (trickName) VALUES ('Pop Shove-it');
+
+-- Intermediate Level Tricks
+INSERT INTO skate_tricks (trickName) VALUES ('Heelflip');
+INSERT INTO skate_tricks (trickName) VALUES ('Varial Kickflip');
+INSERT INTO skate_tricks (trickName) VALUES ('Frontside 180');
+
+-- Advanced Level Tricks
+INSERT INTO skate_tricks (trickName) VALUES ('Backside 180');
+INSERT INTO skate_tricks (trickName) VALUES ('Frontside Boardslide');
+INSERT INTO skate_tricks (trickName) VALUES ('Backside Lipslide');
+
+-- Expert Level Tricks
+INSERT INTO skate_tricks (trickName) VALUES ('360 Flip');
+INSERT INTO skate_tricks (trickName) VALUES ('Hardflip');
+INSERT INTO skate_tricks (trickName) VALUES ('Nollie Heelflip');
+
+-- Pro Level Tricks
+INSERT INTO skate_tricks (trickName) VALUES ('360 Hardflip');
+INSERT INTO skate_tricks (trickName) VALUES ('Varial Heelflip');
+INSERT INTO skate_tricks (trickName) VALUES ('Inward Heelflip');
