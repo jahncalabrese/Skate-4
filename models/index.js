e0ca20026deab075306ef6fc0b3128a4d7b74302
West Marques <donwest@Wests-MacBook-Pro.local>
2024-04-04 21:33:43 -0700

    everything

--- models/index.js
+++ models/index.js
@@ -1,14 +1,14 @@
-const User = require('./users');
-const Trick = require('./Trick');
+const User = require("./user");
+const Trick = require("./skateTrick");
 
 // Define associations
 User.hasMany(Trick, {
-  foreignKey: 'userId', // Name of the foreign key attribute in the Trick model
-  onDelete: 'CASCADE', // When a user is deleted, delete all associated tricks
+  foreignKey: "userId", // Name of the foreign key attribute in the Trick model
+  onDelete: "CASCADE", // When a user is deleted, delete all associated tricks
 });
 
 Trick.belongsTo(User, {
-  foreignKey: 'userId', // Name of the foreign key attribute in the Trick model
+  foreignKey: "userId", // Name of the foreign key attribute in the Trick model
 });
 
 module.exports = { User, Trick };
