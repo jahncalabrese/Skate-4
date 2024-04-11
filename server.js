1cb44ba1ee8e5e3db74c85ef80195caa568f0343
westmarquest <9certified@gmail.com>
2024-04-11 00:53:45 -0700

    hopefully

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 // const helpers = require('./utils/helpers');
+// const tooltip = require("@logo-elements/tooltip");
+// import "@logo-elements/tooltip";
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
